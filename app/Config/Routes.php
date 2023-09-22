ac642d13a09277fcae610a9560b05070ed3b2c8c
deguzmanarieljames <arieljamesdeguzman321@gmail.com>
2023-09-22 22:45:07 +0800

    taposna

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,4 +11,6 @@ $routes->get('/prod/(:any)', 'ProductController::prod/$1');
 $routes->post('/save', 'ProductController::save');
 $routes->get('/delete/(:any)', 'ProductController::delete/$1');
 $routes->get('/edit/(:any)', 'ProductController::edit/$1');
-$routes->get('/search/(:any)', 'ProductController::search/$1');
+$routes->post('/Categorysave', 'ProductController::Categorysave');
+$routes->get('/Categorydelete/(:any)', 'ProductController::Categorydelete/$1');
+$routes->get('/Categoryedit/(:any)', 'ProductController::Categoryedit/$1');
